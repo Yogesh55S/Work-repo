3f8bcd157d8c96f6d8078819482c127904fb0335
anurag262000 <anuragmishra262000@gmail.com>
2025-04-17 10:35:16 +0530

    winter collection image paath error fixing

--- src/Pages/WinterCollection.jsx
+++ src/Pages/WinterCollection.jsx
@@ -1,17 +1,17 @@
-import WinterBanner from '../Component/Winter/WinterBanner';
+import WinterBanner from "../Component/Winter/WinterBanner";
 // import WinterProducts from '../Component/Winter/WinterProducts'
 // import Snowfall from '../Component/Winter/Snowfall'
-import Winter from '../Component/Home/Winter';
+import Winter from "../Component/Home/Winter";
 
 const WinterCollection = () => {
-  return (
-    <div className='pt-20'>
-      {/* <Snowfall/> */}
-      <WinterBanner />
+	return (
+		<div className="pt-20">
+			{/* <Snowfall/> */}
+			<WinterBanner />
 
-      <Winter />
-    </div>
-  );
+			<Winter />
+		</div>
+	);
 };
 
 export default WinterCollection;
